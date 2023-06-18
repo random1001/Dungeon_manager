0e4db1d85a3accdd4e4c1b684adaa375d3228b52
David Beneš <xbenesd01@seznam.cz>
2023-06-18 21:47:53 +0200

    random9: Image browse upgrade

--- gui/image_test_folder.py
+++ gui/image_test_folder.py
@@ -25,12 +25,31 @@ image_label.pack()
 
 def NextPicture():
     global index #Access to global variable
-    index += 1
-    image_label.configure(image=image[index])
     if index == file_count - 1: index=0
+    else: index += 1
+    image_label.configure(image=image[index])
+    Label.configure(text=str(index))
 
-Button = ctk.CTkButton(root, border_color="Yellow", border_width=2, text="Next picture", font=("Roboto", 20), width=200, height=50, command=NextPicture)
-Button.pack(pady=20)
+def PrevPicture():
+    global index #Access to global variable
+    if index == 0: index=file_count-1
+    else: index -= 1
+    image_label.configure(image=image[index])
+    Label.configure(text=str(index))
+
+
+
+Button_prev = ctk.CTkButton(root, border_color="Yellow", border_width=2, text="Previous picture", font=("Roboto", 20), width=200, height=50, command=PrevPicture)
+Button_prev.pack(pady=20, padx=20, side="left")
+
+Button_Next = ctk.CTkButton(root, border_color="Yellow", border_width=2, text="Next picture", font=("Roboto", 20), width=200, height=50, command=NextPicture)
+Button_Next.pack(pady=20, padx=20, side="right")
+
+Counter = ctk.CTkFrame(root, width=40, height=40, fg_color="white")
+Counter.pack(pady=25)
+
+Label = ctk.CTkLabel(Counter, font=("Roboto", 20), text="0", text_color="black")
+Label.pack(pady=5, padx=5)
 
 root.mainloop()
 
